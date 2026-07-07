ae73c7958e152e7e6979eb536a919066af90e087
magnifiquedigitalworld <testingdifferentfunctions@gmail.com>
2026-07-07 10:21:41 +0300

    Add Footer component to PageLayout

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 interface PageLayoutProps {
   children: ReactNode;
@@ -7,13 +8,15 @@ interface PageLayoutProps {
 
 const PageLayout = ({ children }: PageLayoutProps) => {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-background flex flex-col">
       <Header />
-      <main className="container mx-auto px-4 py-8">
+      <main className="container mx-auto px-4 py-8 flex-1">
         {children}
       </main>
+      <Footer />
     </div>
   );
 };
 
 export default PageLayout;
+
